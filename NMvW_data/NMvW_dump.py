0c6d536091c7c052e19ac2fc64c0bd3a63b4cbe3
valevo <valevogelmann@gmx.net>
2021-06-28 11:48:40 +0200

    updated NMvW data dump script

--- NMvW_data/NMvW_dump.py
+++ NMvW_data/NMvW_dump.py
@@ -1,11 +1,14 @@
 from time import time
+from datetime import datetime
 import glob
 from tqdm import tqdm
+import os
 
 import pandas as pd
 import numpy as np
 
 import pyodbc
+
 
 server = 'tcp:azuredfserv.database.windows.net' 
 database = 'Azuredf' 
@@ -31,20 +34,26 @@ def connect_to_DB():
     return pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 
+t0 = time()
 
 with connect_to_DB() as conn:
     q  = "SELECT t.name, t.modify_date FROM sys.tables t"
     tables = pd.read_sql(q, conn)
     tables = tables[tables.name != "Person"]
-    
-    
-    
+
+table_names = list(tables.name)
+
+
+print("about to dump tables:" + "\n\t".join(table_names) + "\n\n")
+
+
+timestamp = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+cur_folder = f"NMvW_tables_{timestamp}"
+os.mkdir(cur_folder)
+
 with connect_to_DB() as conn:
-    for tbl_name in tqdm(tables.name):
+    for tbl_name in tqdm(table_names):
         cur_tbl = table_to_DataFrame(conn, tbl_name, 
                                      keys=None, until=None)
-        cur_tbl.to_csv(f"tables/{tbl_name}.csv.gz", index=False)
-        
-        
-        
+        cur_tbl.to_csv(f"{cur_folder}/{tbl_name}.csv.gz", index=False)
         
